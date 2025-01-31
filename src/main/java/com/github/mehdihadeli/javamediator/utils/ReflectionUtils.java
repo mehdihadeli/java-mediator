0b7addd139dccc1977d2afb93f1156d8747010aa
Mehdi Hadeli <mehdihadeli@gmail.com>
2025-01-31 17:40:24 +0330

    refactor: :recycle: enhance finding response type

--- src/main/java/com/github/mehdihadeli/javamediator/utils/ReflectionUtils.java
+++ src/main/java/com/github/mehdihadeli/javamediator/utils/ReflectionUtils.java
@@ -1,6 +1,8 @@
 package com.github.mehdihadeli.javamediator.utils;
 
 import java.lang.reflect.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ReflectionUtils {
     /**
@@ -11,7 +13,11 @@ public class ReflectionUtils {
      * @return The Class representing the actual generic type argument, or null if not found.
      */
     public static Class<?> getGenericTypeUsingReflection(Object object, Class<?> genericInterface) {
-        for (Type type : object.getClass().getGenericInterfaces()) {
+        // get all base level interfaces for the object
+        Set<Type> interfaces = new HashSet<>();
+        collectInterfaces(object.getClass(), interfaces);
+
+        for (Type type : interfaces) {
             if (type instanceof ParameterizedType parameterizedType) {
                 if (genericInterface.isAssignableFrom((Class<?>) parameterizedType.getRawType())) {
                     Type actualType = parameterizedType.getActualTypeArguments()[0];
@@ -22,5 +28,28 @@ public class ReflectionUtils {
             }
         }
         return null;
+    }
+
+    /**
+     * Helper method to recursively collect interfaces from a class and its hierarchy.
+     */
+    private static void collectInterfaces(Class<?> clazz, Set<Type> interfaces) {
+        if (clazz == null || clazz == Object.class) {
+            return;
+        }
+
+        for (Type genericInterface : clazz.getGenericInterfaces()) {
+            if (interfaces.add(genericInterface)) {
+                if (genericInterface instanceof Class) {
+                    collectInterfaces((Class<?>) genericInterface, interfaces);
+                }
+            }
+        }
+
+        collectInterfaces(clazz.getSuperclass(), interfaces);
+
+        for (Class<?> iface : clazz.getInterfaces()) {
+            collectInterfaces(iface, interfaces);
+        }
     }
 }
